f158d5b79e6ddf14d783bc9f912b58dd9c91f8b7
Harry Fitzgerald <hfitz83@gmail.com>
2023-10-23 21:34:17 +0100

    go fmt

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -3,26 +3,26 @@ package main
 import "fmt"
 
 func main() {
-				rules := map[int]string{
-								3: "fizz",
-								5: "buzz",
-				}
+	rules := map[int]string{
+		3: "fizz",
+		5: "buzz",
+	}
 
-				fizzbuzz(rules, 100)
+	fizzbuzz(rules, 100)
 }
 
 func fizzbuzz(rules map[int]string, max int) {
-				for i := 0; i < max; i += 1 {
-								out := ""
-								for n, s := range rules {
-												if i % n == 0 {
-																out += s
-												}
-								}
-								if len(out) == 0 {
-												fmt.Println(i)
-								} else {
-												fmt.Println(out)
-								}
-				}
+	for i := 0; i < max; i += 1 {
+		out := ""
+		for n, s := range rules {
+			if i%n == 0 {
+				out += s
+			}
+		}
+		if len(out) == 0 {
+			fmt.Println(i)
+		} else {
+			fmt.Println(out)
+		}
+	}
 }
